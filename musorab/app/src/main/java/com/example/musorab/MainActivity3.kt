484aa2698545f02a89772001fd5bce64d7e9f4d6
Yop185 <rostiyoda0505@gmail.com>
2024-05-08 20:30:26 +0300

    обнова 4
    Как-то лень перечислять изменения

--- musorab/app/src/main/java/com/example/musorab/MainActivity3.kt
+++ musorab/app/src/main/java/com/example/musorab/MainActivity3.kt
@@ -1,14 +1,18 @@
 package com.example.musorab
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.provider.Settings
 
 class MainActivity3 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main3)
         }
+    fun OnClick2(view: View){ // Слушатель нажатий
+        val intent = Intent(Settings.ACTION_SETTINGS)
+        startActivity(intent)
+    }
 }
